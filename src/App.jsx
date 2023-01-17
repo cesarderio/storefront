7c7fcef51ace88e316d0f729929662598d5f9489
Raphael Chookagian <raphaelchookagian@gmail.com>
2023-01-16 19:03:06 -0800

    working on lab lab 36 Redux

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Header from './Components/Header';
+import Categories from './Components/Categories'
+import Footer from './Components/Footer';
+
+
+const App = () => {
+  return (
+<>
+<Header />
+<Categories />
+<Footer />
+</>
+  );
+}
+
+export default App;
---
+++ src/store/index.js
@@ -0,0 +1,36 @@
+import { legacy_createStore as createStore, combineReducers } from "redux";
+import { composeWithDevTools } from "@redux-devtools/extension";
+import cartReducer from "./cart";
+import categoriesReducer from "./categories";
+import activeCategoryReducer from "./active-category";
+import productsReducer from "./products";
+
+let reducers = combineReducers({
+  activeCategory: activeCategoryReducer,
+  category: categoriesReducer,
+  products: productsReducer,
+  cart: cartReducer,
+});
+
+let initialState = {
+  categories: [
+    { name: 'electronics', displayName: 'Electronics' },
+    { name: 'food', displayName: 'Food' },
+    { name: 'clothing', displayName: 'Clothing' },
+  ],
+  products: [
+    { name: 'TV', category: 'electronics', price: 699.00, inStock: 5 },
+    { name: 'Radio', category: 'electronics', price: 99.00, inStock: 15 },
+    { name: 'Shirt', category: 'clothing', price: 9.00, inStock: 25 },
+    { name: 'Socks', category: 'clothing', price: 12.00, inStock: 10 },
+    { name: 'Apples', category: 'food', price: .99, inStock: 500 },
+    { name: 'Eggs', category: 'food', price: 1.99, inStock: 12 },
+    { name: 'Bread', category: 'food', price: 2.39, inStock: 90 },
+  ],
+  activeCategory: ''
+};
+
+
+export default function store() {
+  return createStore(reducers, composeWithDevTools());
+}
